7bb09d6db69a9d43bdb51ddfe26fe6d7476e3eb8
Sergio Redondo <rodo1985@gmail.com>
2023-06-21 18:03:29 +0200

    Gradio app, flask and boostrap with tensorflow

--- main.py
+++ main.py
@@ -6,6 +6,49 @@ import time
 from flask import Flask, render_template
 import threading
 import datetime
+import base64
+
+from google.cloud import aiplatform
+from google.cloud.aiplatform.gapic.schema import predict
+
+
+def predict_image_classification_sample(
+    project: str,
+    endpoint_id: str,
+    filename: str,
+    location: str = "us-central1",
+    api_endpoint: str = "us-central1-aiplatform.googleapis.com",
+):
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.gapic.PredictionServiceClient(client_options=client_options)
+    with open(filename, "rb") as f:
+        file_content = f.read()
+
+    # The format of each instance should conform to the deployed model's prediction input schema.
+    encoded_content = base64.b64encode(file_content).decode("utf-8")
+    instance = predict.instance.ImageClassificationPredictionInstance(
+        content=encoded_content,
+    ).to_value()
+    instances = [instance]
+    # See gs://google-cloud-aiplatform/schema/predict/params/image_classification_1.0.0.yaml for the format of the parameters.
+    parameters = predict.params.ImageClassificationPredictionParams(
+        confidence_threshold=0.5, max_predictions=5,
+    ).to_value()
+    endpoint = client.endpoint_path(
+        project=project, location=location, endpoint=endpoint_id
+    )
+    response = client.predict(
+        endpoint=endpoint, instances=instances, parameters=parameters
+    )
+    print("response")
+    print(" deployed_model_id:", response.deployed_model_id)
+    # See gs://google-cloud-aiplatform/schema/predict/prediction/image_classification_1.0.0.yaml for the format of the predictions.
+    predictions = response.predictions
+    for prediction in predictions:
+        print(" prediction:", dict(prediction))
 
 
 def trigger_camera():
@@ -33,8 +76,19 @@ def trigger_camera():
     # creates date time
     date_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
+    # filename
+    filename = os.path.join('images', f"image_{date_time}.png")
+
     # Save the image with the date and time in the filename
-    cv2.imwrite(os.path.join('images', f"image_{date_time}.png"), img)
+    cv2.imwrite(filename, img)
+
+    # image prediction
+    predict_image_classification_sample(
+    project="795715818331",
+    endpoint_id="1477985520289054720",
+    location="us-central1",
+    filename=filename
+    )
 
     return img
 
